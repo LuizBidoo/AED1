8fb0fa7a725417b19ea59e418dd83df9c3f5c792
Luiz Bido <136762529+LuizBidoo@users.noreply.github.com>
2023-08-27 19:49:30 -0300

    Formatação modificada

--- pratica3_aed1_oficial/src/main.c
+++ pratica3_aed1_oficial/src/main.c
@@ -7,78 +7,78 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
-void compare( struct TreeNode* root, int* minimumVal, int* secondMinimum ) {
-    if ( root == NULL ) { // caso em que não existe
+void compare( struct TreeNode* root, int* minimumVal, int* secondMinimum ){
+    if ( root == NULL ){ // caso em que não existe
         return;
     }
-    if (*minimumVal == -1) { // para pegar o primeiro menor valor
+    if ( *minimumVal == -1 ){ // para pegar o primeiro menor valor
         *minimumVal = root->val;
-    } else if (root->val < *minimumVal) { // para pegar um novo menor valor
+    }else if ( root->val < *minimumVal ) { // para pegar um novo menor valor
         *secondMinimum = *minimumVal;
         *minimumVal = root->val;
-    } else if (root->val > *minimumVal && (*secondMinimum == -1 || root->val < *secondMinimum)) { // teste para ver se existe um novo segundo menor valor
+    }else if ( root->val > *minimumVal && ( *secondMinimum == -1 || root->val < *secondMinimum ) ){ // teste para ver se existe um novo segundo menor valor
         *secondMinimum = root->val;
     }
-    compare(root->left, minimumVal, secondMinimum); // recursividade para procurar à esquerda
-    compare(root->right, minimumVal, secondMinimum); // recursividade para procurar à direita
+    compare( root->left, minimumVal, secondMinimum ); // recursividade para procurar à esquerda
+    compare( root->right, minimumVal, secondMinimum ); // recursividade para procurar à direita
 }
 
-int findSecondMinimumValue(struct TreeNode* root) {
+int findSecondMinimumValue( struct TreeNode* root ){
     int minimumVal = -1;
     int secondMinimum = -1;
-    compare(root, &minimumVal, &secondMinimum);
+    compare( root, &minimumVal, &secondMinimum );
 
-    if (secondMinimum == -1) {
+    if ( secondMinimum == -1 ){
         return -1;
-    } else {
+    }else{
         return secondMinimum;
     }
 }
 
 
-struct TreeNode* insert(struct TreeNode* root, int val) {
-    if (root == NULL) {
-        struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+struct TreeNode* insert( struct TreeNode* root, int val ){
+    if ( root == NULL ) {
+        struct TreeNode* newNode = ( struct TreeNode* )malloc( sizeof( struct TreeNode ) );
         newNode->val = val;
         newNode->left = NULL;
         newNode->right = NULL;
         return newNode;
     }
 
-    if (val < root->val) {
-        root->left = insert(root->left, val);
-    } else if (val > root->val) {
-        root->right = insert(root->right, val);
+    if ( val < root->val ){
+        root->left = insert( root->left, val );
+    }else if ( val > root->val ){
+        root->right = insert( root->right, val );
     }
     
     return root;
 }
 
-void destroy(struct TreeNode* root) {
-    if (root == NULL) {
+void destroy( struct TreeNode* root ){
+    if (root == NULL){
         return;
     }
     
-    destroy(root->left);
-    destroy(root->right);
-    free(root);
+    destroy( root->left );
+    destroy( root->right );
+    free( root );
 }
 
 int main() {
     struct TreeNode* root = NULL;
 
-    root = insert(root, 8);
-    root = insert(root, 4);
-    root = insert(root, 12);
-    root = insert(root, 2);
-    root = insert(root, 6);
-    root = insert(root, 10);
-    root = insert(root, 14);
+    root = insert( root, 8 );
+    root = insert( root, 4 );
+    root = insert( root, 12 );
+    root = insert( root, 2 );
+    root = insert( root, 6 );
+    root = insert( root, 10 );
+    root = insert( root, 14 );
 
-    int secondMin = findSecondMinimumValue(root);
-    printf("segundo menor: %d\n", secondMin); 
+    int secondMin = findSecondMinimumValue( root );
+    printf( "segundo menor: %d\n", secondMin ); 
 
-    destroy(root);
+    destroy( root );
 
     return 0;
 }
